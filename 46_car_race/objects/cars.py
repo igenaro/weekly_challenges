6942b7c3fe98931623f170bdc9f679e62baad7ec
IvanSalinas <sp.igenaro@gmail.com>
2023-12-12 22:46:45 -0600

    terminado reto 46

--- 46_car_race/objects/cars.py
+++ 46_car_race/objects/cars.py
@@ -2,9 +2,21 @@ import random
 
 class Cars:
 
-    def __init__(self, color, mileage = 0):
+    def __init__(self, color, icon, mileage = 0, winner = False, working = True):
         self.color = color
         self.mileage = mileage
+        self.icon = icon
+        self.winner = winner
+        self.working = working
 
     def advance(self):
         self.mileage += random.randint(1, 3)
+    
+    def winner_flag(self):
+        self.winner = True
+    
+    def crash(self):
+        self.working = False
+
+    def repair(self):
+        self.working = True
---
+++ 46_car_race/objects/pista.py
@@ -0,0 +1,31 @@
+import random
+
+class Pista:
+
+    def __init__(self, length, car_icon):
+        self.length = length
+        self.car_icon = car_icon
+
+    
+    # Inicia una pista de longitud deseada
+    def init_pista(self):
+        pista_string = "_"*self.length
+        return pista_string
+    
+    # Imprime el carro y los arboles en la pista.
+    # Falta agregar que dos arboles no se sobrepongan
+    def cars_and_trees_in_road(self, road):
+        num_trees = random.randint(1, 3)
+        l = len(road)
+        road_list = list(road)
+        for i in range(num_trees):
+            pos_tree = random.randint(1,l-2)
+            road_list[pos_tree] = "T"
+        road = ''.join(road_list)
+        return road
+
+    
+    def run(self):
+        road = self.init_pista()
+        road = self.cars_and_trees_in_road(road)
+        return road
